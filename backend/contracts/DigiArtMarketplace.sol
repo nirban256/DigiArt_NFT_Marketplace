f333cb467efe01f79f005b88ff80c24a8bb4baad
NIRBAN CHAKRABORTY <nirban256@gmail.com>
2024-02-03 02:28:22 +0530

    added resale of NFTs logic in the same contract

--- backend/contracts/DigiArtMarketplace.sol
+++ backend/contracts/DigiArtMarketplace.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 
 /* 
@@ -15,13 +15,14 @@ import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
     way to find all the nfts owned by a user - using opensea api - https://docs.opensea.io/reference/api-overview
 */
 
-contract DigiArtMarketplace is ERC721, Ownable, ReentrancyGuard, Pausable {
-    ERC721 nft;
+contract DigiArtMarketplace is Ownable, ReentrancyGuard, Pausable {
+    IERC721 nft;
 
     uint256 private _listedTokenIdCounter;
     uint256 private _soldTokenIdCounter;
+    uint256 private _listedTokenIdForResaleCounter;
 
-    uint256 public listingFee;
+    uint256 private listingFee;
 
     address payable admin;
 
@@ -36,7 +37,19 @@ contract DigiArtMarketplace is ERC721, Ownable, ReentrancyGuard, Pausable {
         bool sold;
     }
 
-    mapping(uint256 => Item) public _idToMarketItem;
+    struct ResellItem {
+        uint256 id;
+        address nftContract;
+        uint256 tokenId;
+        address payable sellerOfTokenId;
+        address payable ownerOfTokenId;
+        address payable buyerOfTokenId;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(address => mapping(uint256 => Item)) public _idToMarketItem;
+    mapping(uint256 => ResellItem) public _idToResellMarketItem;
 
     event MarketItemCreated(
         address indexed sellerOfTokenId,
@@ -50,15 +63,134 @@ contract DigiArtMarketplace is ERC721, Ownable, ReentrancyGuard, Pausable {
         uint256 indexed price
     );
 
-    constructor(
-        uint256 _listingFee
-    ) Ownable(msg.sender) ERC721("DigiArtMarketplace", "DAM") {
+    event MarketItemCreatedForResale(
+        address indexed sellerOfTokenId,
+        address indexed buyerOfTokenId,
+        uint256 indexed price
+    );
+
+    constructor(uint256 _listingFee) Ownable(msg.sender) {
         require(msg.sender != address(0), "Invalid address");
         require(_listingFee > 0, "Listing fee must be greater than 0");
         admin = payable(msg.sender);
+        listingFee = _listingFee;
+        _listedTokenIdCounter = 0;
+        _soldTokenIdCounter = 0;
+        _listedTokenIdForResaleCounter = 0;
     }
 
-    function listForMarketSale(
+    function listNFTResale(
+        address _nftContract,
+        uint256 _tokenId,
+        uint256 _price
+    ) public payable nonReentrant whenNotPaused {
+        require(_price > 0 ether, "Price must be greater than 0 ether");
+        require(
+            msg.value == listingFee,
+            "Not enough balance to complete transaction"
+        );
+        nft = IERC721(_nftContract);
+        require(
+            nft.ownerOf(_tokenId) == msg.sender ||
+                nft.getApproved(_tokenId) == msg.sender,
+            "You are not the owner of this token"
+        );
+
+        Item memory NFT = getPreSaleNFT(_nftContract, _tokenId);
+
+        require(
+            NFT.id == 0,
+            "NFT not traded before hence it needs to be traded first"
+        );
+        _listedTokenIdForResaleCounter += 1;
+
+        uint256 _id = _listedTokenIdForResaleCounter;
+
+        _idToResellMarketItem[_id] = ResellItem(
+            _id,
+            _nftContract,
+            _tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            payable(address(0)),
+            _price,
+            false
+        );
+
+        nft.safeTransferFrom(msg.sender, address(this), _tokenId);
+        // ERC721(_nftContract).approve(address(this), _tokenId);
+
+        emit MarketItemCreatedForResale(msg.sender, address(0), _price);
+    }
+
+    function createNFTResale(
+        address _nftContract,
+        uint256 _itemId
+    ) public payable nonReentrant whenNotPaused {
+        uint price = _idToResellMarketItem[_itemId].price;
+        uint tokenId = _idToResellMarketItem[_itemId].tokenId;
+        require(
+            msg.value == price,
+            "Not enough balance to complete transaction"
+        );
+        require(
+            _idToResellMarketItem[_itemId].sellerOfTokenId != msg.sender,
+            "You cannot buy your own token"
+        );
+        require(
+            _idToResellMarketItem[_itemId].sold == true,
+            "Token already sold"
+        );
+        require(
+            _idToResellMarketItem[_itemId].tokenId >= 0,
+            "Token not listed"
+        );
+
+        _idToResellMarketItem[_itemId].buyerOfTokenId = payable(msg.sender);
+        _idToResellMarketItem[_itemId].ownerOfTokenId = payable(msg.sender);
+        _idToResellMarketItem[_itemId].sold = true;
+
+        _soldTokenIdCounter += 1;
+
+        nft = IERC721(_nftContract);
+
+        (bool success, ) = address(this).call{value: price}("");
+        require(success, "Transfer failed while buying token");
+
+        nft.safeTransferFrom(address(this), msg.sender, tokenId);
+
+        (success, ) = _idToResellMarketItem[_itemId].sellerOfTokenId.call{
+            value: price
+        }("");
+        require(success, "Transfer failed while sending money to seller");
+
+        emit MarketItemSold(
+            _idToResellMarketItem[_itemId].sellerOfTokenId,
+            _idToResellMarketItem[_itemId].buyerOfTokenId,
+            _idToResellMarketItem[_itemId].price
+        );
+    }
+
+    function cancelNFTResale(
+        uint256 _tokenId
+    ) public nonReentrant whenNotPaused {
+        require(
+            _idToResellMarketItem[_tokenId].sellerOfTokenId == msg.sender ||
+                _idToResellMarketItem[_tokenId].ownerOfTokenId == msg.sender,
+            "You don't have the required permission"
+        );
+        require(
+            _idToResellMarketItem[_tokenId].sold == true,
+            "Token already sold"
+        );
+
+        nft = IERC721(_idToResellMarketItem[_tokenId].nftContract);
+        nft.safeTransferFrom(address(this), msg.sender, _tokenId);
+
+        delete _idToResellMarketItem[_tokenId];
+    }
+
+    function listNFTForMarketSale(
         address _nftContract,
         uint256 _tokenId,
         uint256 _price
@@ -69,16 +201,17 @@ contract DigiArtMarketplace is ERC721, Ownable, ReentrancyGuard, Pausable {
             "Not enough balance to complete transaction"
         );
 
-        nft = ERC721(_nftContract);
+        nft = IERC721(_nftContract);
         require(
             nft.ownerOf(_tokenId) == msg.sender ||
                 nft.getApproved(_tokenId) == msg.sender,
             "You are not the owner of this token"
         );
 
+        _listedTokenIdCounter += 1;
         uint256 _id = _listedTokenIdCounter;
 
-        _idToMarketItem[_tokenId] = Item(
+        _idToMarketItem[_nftContract][_tokenId] = Item(
             _id,
             _nftContract,
             _tokenId,
@@ -88,7 +221,6 @@ contract DigiArtMarketplace is ERC721, Ownable, ReentrancyGuard, Pausable {
             _price,
             false
         );
-        _listedTokenIdCounter += 1;
 
         nft.safeTransferFrom(msg.sender, address(this), _tokenId);
         // ERC721(_nftContract).approve(address(this), _tokenId);
@@ -96,62 +228,86 @@ contract DigiArtMarketplace is ERC721, Ownable, ReentrancyGuard, Pausable {
         emit MarketItemCreated(msg.sender, address(0), _price);
     }
 
-    function createMarketSale(
-        address nftContract,
+    function createNFTMarketSale(
+        address _nftContract,
         uint256 _itemId
     ) public payable nonReentrant whenNotPaused {
-        uint price = _idToMarketItem[_itemId].price;
-        uint tokenId = _idToMarketItem[_itemId].tokenId;
+        uint price = _idToMarketItem[_nftContract][_itemId].price;
+        uint tokenId = _idToMarketItem[_nftContract][_itemId].tokenId;
         require(
             msg.value == price,
             "Not enough balance to complete transaction"
         );
         require(
-            _idToMarketItem[_itemId].sellerOfTokenId != msg.sender,
+            _idToMarketItem[_nftContract][_itemId].sellerOfTokenId !=
+                msg.sender,
             "You cannot buy your own token"
         );
-        require(_idToMarketItem[_itemId].sold == true, "Token already sold");
-        require(_idToMarketItem[_itemId].tokenId >= 0, "Token not listed");
+        require(
+            _idToMarketItem[_nftContract][_itemId].sold == true,
+            "Token already sold"
+        );
+        require(
+            _idToMarketItem[_nftContract][_itemId].tokenId >= 0,
+            "Token not listed"
+        );
 
-        _idToMarketItem[_itemId].buyerOfTokenId = payable(msg.sender);
-        _idToMarketItem[_itemId].ownerOfTokenId = payable(msg.sender);
-        _idToMarketItem[_itemId].sold = true;
+        _idToMarketItem[_nftContract][_itemId].buyerOfTokenId = payable(
+            msg.sender
+        );
+        _idToMarketItem[_nftContract][_itemId].ownerOfTokenId = payable(
+            msg.sender
+        );
+        _idToMarketItem[_nftContract][_itemId].sold = true;
 
         _soldTokenIdCounter += 1;
 
-        nft = ERC721(nftContract);
+        nft = IERC721(_nftContract);
 
         (bool success, ) = address(this).call{value: price}("");
         require(success, "Transfer failed while buying token");
 
         nft.safeTransferFrom(address(this), msg.sender, tokenId);
 
-        (success, ) = _idToMarketItem[_itemId].sellerOfTokenId.call{
-            value: price
-        }("");
+        (success, ) = _idToMarketItem[_nftContract][_itemId]
+            .sellerOfTokenId
+            .call{value: price}("");
         require(success, "Transfer failed while sending money to seller");
 
         emit MarketItemSold(
-            _idToMarketItem[_itemId].sellerOfTokenId,
-            _idToMarketItem[_itemId].buyerOfTokenId,
-            _idToMarketItem[_itemId].price
+            _idToMarketItem[_nftContract][_itemId].sellerOfTokenId,
+            _idToMarketItem[_nftContract][_itemId].buyerOfTokenId,
+            _idToMarketItem[_nftContract][_itemId].price
         );
     }
 
-    function cancelMarketSale(
+    function cancelNFTMarketSale(
+        address _nftContract,
         uint256 _tokenId
     ) public nonReentrant whenNotPaused {
         require(
-            _idToMarketItem[_tokenId].sellerOfTokenId == msg.sender ||
-                _idToMarketItem[_tokenId].ownerOfTokenId == msg.sender,
+            _idToMarketItem[_nftContract][_tokenId].sellerOfTokenId ==
+                msg.sender ||
+                _idToMarketItem[_nftContract][_tokenId].ownerOfTokenId ==
+                msg.sender,
             "You don't have the required permission"
         );
-        require(_idToMarketItem[_tokenId].sold == true, "Token already sold");
+        require(
+            _idToMarketItem[_nftContract][_tokenId].sold == true,
+            "Token already sold"
+        );
 
-        nft = ERC721(_idToMarketItem[_tokenId].nftContract);
+        nft = IERC721(_idToMarketItem[_nftContract][_tokenId].nftContract);
         nft.safeTransferFrom(address(this), msg.sender, _tokenId);
 
-        delete _idToMarketItem[_tokenId];
+        delete _idToMarketItem[_nftContract][_tokenId];
+    }
+
+    function getPreSaleNFT(
+        address _nftContract,
+        uint256 _tokenId
+    ) public view returns (Item memory) {
+        return _idToMarketItem[_nftContract][_tokenId];
     }
 
     function getListingFee() public view returns (uint256) {
