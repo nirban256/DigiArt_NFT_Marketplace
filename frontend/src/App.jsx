153c218008acd79f39b5feadfebb89d5f8789d7a
NIRBAN CHAKRABORTY <nirban256@gmail.com>
2024-02-20 22:03:42 +0530

    added light and dark modes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,47 @@
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Collections from './components/Collections';
 
 const App = () => {
+  const [mode, setMode] = useState('dark-mode');
+  const toggleTheme = () => {
+    if (mode === 'dark-mode') {
+      setMode('light-mode');
+    } else {
+      setMode('dark-mode');
+    }
+  }
+
+  useEffect(() => {
+    document.body.className = mode;
+  }, [mode]);
+
   return (
-    <div className='base-design bg-[#413eea]'>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collections />} />
-      </Routes>
-    </div>
+    <ThirdwebProvider
+      activeChain="mumbai"
+      clientId={process.env.REACT_APP_THIRDWEB_CLIENT_ID}
+      supportedWallets={[
+        metamaskWallet({ recommended: true }),
+        coinbaseWallet(),
+        walletConnect(),
+      ]}>
+      <div className={`App ${mode}`}>
+        <Header toggleTheme={toggleTheme} mode={mode} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collections />} />
+        </Routes>
+      </div>
+    </ThirdwebProvider>
   );
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
+import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
+import { ConnectWallet } from "@thirdweb-dev/react";
 import { Link } from 'react-router-dom';
 import logo from "../images/logo.png";
 
-const Header = () => {
+const Header = ({ toggleTheme, mode }) => {
+
+    const handleTheme = () => {
+        if (mode === 'dark-mode') {
+            return 'dark';
+        } else {
+            return 'light';
+        }
+    }
+
     return (
         <header className='flex justify-between'>
             <img src={logo} alt="DigiArt Marketplace" className='logo' />
             <div>
-                <ul className='flex justify-between text-lg underline'>
-                    <li className=' p-3'>
+                <ul className='flex justify-between text-lg p-3 cursor-pointer'>
+                    <button className='btn p-3' onClick={toggleTheme}>
+                        {mode === 'dark-mode' ? <MdOutlineDarkMode size={25} /> : <MdDarkMode size={25} color='white' />}
+                    </button>
+                    <li className=' p-3 underline'>
                         <Link to="/">Home</Link>
                     </li>
-                    <li className='p-3'>
+                    <li className='p-3 underline'>
                         <Link to="/collection">Marketplace</Link>
                     </li>
+                    <ConnectWallet
+                        theme={handleTheme()}
+                        switchToActiveChain={true}
+                        modalSize={"compact"}
+                        showThirdwebBranding={false}
+                    />
                 </ul>
             </div>
         </header>
     )
 }
 
-export default Header
+export default Header;
